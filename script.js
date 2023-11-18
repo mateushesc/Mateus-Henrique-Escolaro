ad6bdc1e466f0bf1d6e4e4236333d665c58775bc
Mateus_Escolaro <matescolaro@gmail.com>
2023-11-18 19:04:44 -0300

    Mais mudanças

--- script.js
+++ script.js
@@ -1,23 +1,10 @@
-
-document.querySelectorAll("nav ul a").forEach(link =>{
-    link.addEventListener('click', evento =>{
-        evento.preventDefault();
-        const href = link.getAttribute("href");
-        const alvo = document.querySelector(href);
-        if(alvo){
-            alvo.scrollIntoView({behavior: "smooth"});
-        }
-    });
-});
-
-
 const menu = document.getElementById("menu");
 const scrollLimite = 100;
 
-window.addEventListener("scroll", () =>{
-    if(window.scrollY > scrollLimite){
+window.addEventListener("scroll", () => {
+    if (window.scrollY > scrollLimite) {
         menu.classList.add("shrink");
-    }else{
+    } else {
         menu.classList.remove("shrink");
     }
 })
