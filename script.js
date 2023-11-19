876080a00f21d94d547136e5d0149d58826e792e
Mateus_Escolaro <matescolaro@gmail.com>
2023-11-18 21:08:04 -0300

    Corrigindo algumas coisas e adição de função com scroll

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const menu = document.getElementById("menu");
+const scrollLimite = 100;
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > scrollLimite) {
+      menu.classList.add("shrink");
+  } else {
+      menu.classList.remove("shrink");
+  }
+});
+
 function salvar() {
   const nome = document.getElementById('nome').value;
   const cpf = document.getElementById('cpf').value;
